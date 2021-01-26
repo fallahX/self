ed655c5eb53fd890f9d1ed759f0e20d22ee965f5
fallahX <75850671+fallahX@users.noreply.github.com>
2021-01-26 08:06:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ hafizh.on('group-participants-update', async (anu) => {
 				}
 			}
 			const botNumber = hafizh.user.jid
-			const ownerNumber = ["62895414817057@s.whatsapp.net"] // ganti nomer lu
+			const ownerNumber = ["6289514468104@s.whatsapp.net"] // ganti nomer lu
 			const isGroup = from.endsWith('@g.us')
 			const sender = isGroup ? tod.participant : tod.key.remoteJid
 			const groupMetadata = isGroup ? await hafizh.groupMetadata(from) : ''
